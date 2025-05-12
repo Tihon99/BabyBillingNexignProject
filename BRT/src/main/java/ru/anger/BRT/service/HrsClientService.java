221052a1c3b8c0c4392be1a919855275c3e8cc63
Tihon99 <tihon_baikal_99@mail.ru>
2025-05-12 08:28:08 +0300

    add H2, edit HrsClientService and fixed pom.xml

--- BRT/src/main/java/ru/anger/BRT/service/HrsClientService.java
+++ BRT/src/main/java/ru/anger/BRT/service/HrsClientService.java
@@ -6,17 +6,28 @@ import org.springframework.web.client.RestClient;
 import ru.anger.BRT.DTO.DataForRatingDTO;
 import ru.anger.BRT.DTO.ChargeResultDTO;
 
+import java.math.BigDecimal;
+
 @Service
 public class HrsClientService {
 
     @Autowired
     private RestClient restClient;
 
-    public ChargeResultDTO calculateCharge(DataForRatingDTO dto) {
+    /*public ChargeResultDTO calculateCharge(DataForRatingDTO dto) {
         return restClient.post()
                 .uri("/calculate")
                 .body(dto)
                 .retrieve()
                 .body(ChargeResultDTO.class);
+    }*/
+
+    public ChargeResultDTO calculateCharge(DataForRatingDTO dto) {
+        // Заглушка: возвращаем фиксированный результат
+        return new ChargeResultDTO(
+                BigDecimal.valueOf(5.00), // chargeAmount
+                5,                        // minutesSpent
+                false                     // shouldUpdateLastPaymentDate
+        );
     }
 }
